0546934077b70cabfbbc1abbd1ee319657c4323c
N4thanJ <nathan.jordens@student.ucll.be>
2025-11-04 11:11:49 +0100

    init

---
+++ src/test/java/be/ucll/design_pattern_exercises/DesignPatternExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll.design_pattern_exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
